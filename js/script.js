4bc07dedb2498c3d511b7a765ca06ee92055c6b7
ekorostianto <eko.rostian@gmail.com>
2023-03-02 23:18:55 +0700

    add wp posts feed

--- js/script.js
+++ js/script.js
@@ -1,10 +1,10 @@
 $(document).ready(function() {
     $.ajax({
-        url: "https://www.googleapis.com/blogger/v3/blogs/8957823820112248820/posts?key=AIzaSyAiA49m0zyriCWHl5D1ChpwKVaVcpzy_kA"
+        url: "https://surabayasurveysolution.com/blog/wp-json/wp/v2/posts?per_page=12&orderby=date"
     }).then(function(data) {
         
-        for (i = 0; i < data.items.length; i++) {
-          $('#posts').append("<li><article><div class='uk-card uk-card-primary uk-card-hover'><div class='uk-card-body'><div class='uk-card-badge'><span class='uk-label'>"+data.items[i].labels+"</span></div><h1 class='uk-h2'><a href="+data.items[i].url+">"+data.items[i].title+"</a></h1></div><div class='uk-card-footer'><p class='uk-text-meta'>Written by "+data.items[i].author.displayName+" on <time datetime='"+data.items[i].updated+"'>"+data.items[i].updated+"</time>.</p></div></div></article></li>");
+        for (i = 0; i < data.length; i++) {
+          $('#posts').append("<li><article><div class='uk-card uk-card-primary uk-card-hover'><div class='uk-card-body'><div class='uk-card-badge'><span class='uk-label'>"+data[i].categories+"</span></div><h1 class='uk-h2'><a href="+data[i].slug+">"+data[i].title.rendered+"</a></h1></div><div class='uk-card-footer'><p class='uk-text-meta'>Written by <a href='"+data[i].uagb_author_info.author_link+"'>"+data[i].uagb_author_info.display_name+"</a>"+" on <time datetime='"+data[i].modified+"'>"+data[i].modified+"</time>.</p></div></div></article></li>");
         }
     });
 });
