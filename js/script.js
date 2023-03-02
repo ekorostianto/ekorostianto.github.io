a16f355d17078a630d3b806774310421ec749297
ekorostianto <eko.rostian@gmail.com>
2023-03-02 23:24:39 +0700

    fixed slug

--- js/script.js
+++ js/script.js
@@ -4,7 +4,7 @@ $(document).ready(function() {
     }).then(function(data) {
         
         for (i = 0; i < data.length; i++) {
-          $('#posts').append("<li><article><div class='uk-card uk-card-primary uk-card-hover'><div class='uk-card-body'><div class='uk-card-badge'><span class='uk-label'>"+data[i].categories+"</span></div><h1 class='uk-h2'><a href="+data[i].slug+">"+data[i].title.rendered+"</a></h1></div><div class='uk-card-footer'><p class='uk-text-meta'>Written by <a href='"+data[i].uagb_author_info.author_link+"'>"+data[i].uagb_author_info.display_name+"</a>"+" on <time datetime='"+data[i].modified+"'>"+data[i].modified+"</time>.</p></div></div></article></li>");
+          $('#posts').append("<li><article><div class='uk-card uk-card-primary uk-card-hover'><div class='uk-card-body'><div class='uk-card-badge'><span class='uk-label'>"+data[i].categories+"</span></div><h1 class='uk-h2'><a href='/blog/"+data[i].slug+"'>"+data[i].title.rendered+"</a></h1></div><div class='uk-card-footer'><p class='uk-text-meta'>Written by <a href='"+data[i].uagb_author_info.author_link+"'>"+data[i].uagb_author_info.display_name+"</a>"+" on <time datetime='"+data[i].modified+"'>"+data[i].modified+"</time>.</p></div></div></article></li>");
         }
     });
 });
